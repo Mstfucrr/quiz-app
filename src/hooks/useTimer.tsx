55537b6cb6b3f48fd55db9e7ec51fcdaa390a064
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2024-06-10 11:01:20 +0300

    Add startTimer and stopTimer functions to useTimer hook

--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -1,18 +1,31 @@
-// src/hooks/useTimer.tsx
-
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 const useTimer = () => {
   const [timer, setTimer] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+
+  // Using useRef to store the interval reference
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
 
   useEffect(() => {
-    const interval = setInterval(() => setTimer(prev => prev + 1), 1000)
-    return () => clearInterval(interval)
-  }, [])
+    if (isRunning) {
+      // If the timer is running, set the interval to increment the timer by 1 every second
+      intervalRef.current = setInterval(() => setTimer(prev => prev + 1), 1000)
+    } else if (!isRunning && intervalRef.current !== null) {
+      // If the timer is not running and the interval reference is not null, clear the interval
+      clearInterval(intervalRef.current)
+      intervalRef.current = null
+    }
+    return () => { // Clear the interval when the component unmounts
+      if (intervalRef.current) clearInterval(intervalRef.current) 
+    }
+  }, [isRunning])
 
+  const startTimer = () => setIsRunning(true)
+  const stopTimer = () => setIsRunning(false)
   const resetTimer = () => setTimer(0)
 
-  return { timer, resetTimer }
+  return { timer, startTimer, stopTimer, resetTimer, isRunning }
 }
 
 export default useTimer
